4fd77fa030a91f450d6fc258f665c4354bc6e5cc
Justin Jarae <lawnjarae@gmail.com>
2025-01-23 17:35:46 -0500

    Removed need for key.

--- main.tf
+++ main.tf
@@ -31,24 +31,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = file("${path.module}/key.pub")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("${path.module}/key.pub")
+# }
 
 resource "aws_instance" "example" {
   ami                    = data.aws_ami.ubuntu.id
-  key_name               = aws_key_pair.deployer.key_name
+  # key_name               = aws_key_pair.deployer.key_name
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg_ssh.id]
-  user_data              = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+  # user_data              = <<-EOF
+  #             #!/bin/bash
+  #             apt-get update
+  #             apt-get install -y apache2
+  #             sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+  #             echo "Hello World" > /var/www/html/index.html
+  #             systemctl restart apache2
+  #             EOF
   tags = {
     Name          = "terraform-learn-state-ec2"
     drift_example = "v1"
